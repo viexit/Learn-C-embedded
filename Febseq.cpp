a991ed781b985ac83b4da5266f4fc4505441b21f
viexit <shamieh123@gmail.com>
2025-04-04 02:41:37 -0500

    Fibonacci Sequence - while loop

---
+++ Febseq.cpp
@@ -0,0 +1,31 @@
+/* Febonachi sequence in while loop
+    n = 0,1,2,3,4,5,6,7,8,9
+    xn = 0,1,1,2,3,5,8,13,21,34
+
+*/
+#include <stdio.h>
+
+int main()
+{
+    int limit;
+    int count=0;
+    int i=0;
+    int j=1;
+    int k;
+    printf("Enter the limit: ");
+    scanf("%d",&limit);
+    while (i < limit)
+    {
+        if (i==0)
+            i = i + j;
+        else if (i==1)
+        
+            i = i + j;
+            k = i; 
+    printf("Febonachin sequence is f(%d) is:%d\n",count,i=k+j);
+    count++;
+    j=k;
+    k=i;
+    }  
+return 0;
+}
